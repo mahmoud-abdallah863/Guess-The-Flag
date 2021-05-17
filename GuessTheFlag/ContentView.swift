cc0f13fee00efd00abd5ab969f50e63498b7eb6c
Mahmoud Abdallah <mabdallah863@gmail.com>
2021-05-18 01:46:59 +0300

    Learned about gradient colors, HStack, VStack and ZStack

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+//        ZStack {
+//            // This will take a space and change the background color of it
+//
+//            // In this case, we are using ZStack, so the color will be in z=0 and text in z=1
+//            // But if HStack is used this will result in a little vertical space for the text, and the rest will have a gray background color
+//            Color.gray.frame(width:200, height: 200) // We cna also set the width and height
+//            Text("This is a text")
+//        }
+        
+        
+//        // Gradient background colors
+//        ZStack {
+//            LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing).ignoresSafeArea()
+//            
+//            RadialGradient(gradient: Gradient(colors: [Color.black.opacity(0.6), Color.red.opacity(0.0)]), center: .center, startRadius: 5, endRadius: 200)
+//        }
+        
+//        // Angular gradeint background color
+//        ZStack {
+//            AngularGradient(gradient: Gradient(colors: [.red, .blue, .green, .yellow, .pink, .red]), center: .center)
+//        }
+        
+        
+//        VStack(spacing: 10) {
+//
+//            HStack {
+//                Text("1").padding()
+//                Text("2").padding()
+//                Text("3").padding()
+//            }
+//
+//            Text("First").padding()
+//            Text("Second").padding()
+//            Text("Third").padding()
+//        }
     }
 }
 
