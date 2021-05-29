73045a2c82f20bd81b4e614dfdd40581a72d4cc5
Mahmoud Abdallah <mabdallah863@gmail.com>
2021-05-30 02:06:59 +0300

    Updated the ui, and set game flow

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,12 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    
-    @State private var showingAlter: Bool = false
-    
-    var body: some View {
-        
+//      Old stuff
 //        ZStack {
 //            // This will take a space and change the background color of it
 //
@@ -36,31 +31,107 @@ struct ContentView: View {
 //        }
         
         
-        VStack(spacing: 10) {
+//VStack(spacing: 10) {
+//
+//    HStack {
+//        Text("1").padding()
+//        Text("2").padding()
+//        Text("3").padding()
+//    }
+//
+//    Text("First").padding()
+//    Text("Second").padding()
+//    Text("Third").padding()
+//
+//    Button(action: {
+//        print("Hello World")
+//    }) {
+//        Text("Click me")
+//    }
+//
+//    // Show an alert when button is clicked
+//    Button("Show alert") {
+//        self.showingAlter = true
+//    }.alert(isPresented: $showingAlter) {
+//        Alert(title: Text("This is a title"), message: Text("This is some detail"), dismissButton: .default(Text("OK")))
+//    }
+//}
 
-            HStack {
-                Text("1").padding()
-                Text("2").padding()
-                Text("3").padding()
-            }
 
-            Text("First").padding()
-            Text("Second").padding()
-            Text("Third").padding()
+
+struct ContentView: View {
+    
+    @State var countries = ["Estonia", "France", "Germany", "Italy", "Russia", "Spain", "UK", "US"].shuffled()
+    @State var currentCountryIndex = Int.random(in: 0...2)
+    
+    @State var showingResult: Bool = false
+    @State var score: Int = 0
+    @State var scoreTitle: String = ""
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.red, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
-            Button(action: {
-                print("Hello World")
-            }) {
-                Text("Click me")
+            VStack(spacing: 30) {
+                Spacer().frame(width: .none, height: 30, alignment: .center)
+
+                VStack {
+                    Text("Tap the flag of")
+                        .italic()
+                        .foregroundColor(.white).font(.headline)
+                    
+                    Text(countries[currentCountryIndex])
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .shadow(color: .black, radius: 10, x: 0.0, y: 0.0)
+                }
+                
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        let correctAnswer = self.flagTapped(flagIndex: number)
+                        
+                        if correctAnswer {
+                            self.askQuestion()
+                        }else {
+                            self.showingResult = true
+                        }
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .shadow(color: .black, radius: 10)
+                            
+                    }
+                }
+                Spacer()
+                
+                Text("Your score is \(self.score)")
+                    .foregroundColor(.white)
             }
-            
-            // Show an alert when button is clicked
-            Button("Show alert") {
-                self.showingAlter = true
-            }.alert(isPresented: $showingAlter) {
-                Alert(title: Text("This is a title"), message: Text("This is some detail"), dismissButton: .default(Text("OK")))
-            }
+            .alert(isPresented: $showingResult, content: {
+                Alert(title: Text(self.scoreTitle), message: nil,
+                      dismissButton: .default(Text("Continue")) {
+                        self.askQuestion()
+                      }
+                )
+            })
         }
+    }
+    
+    func flagTapped(flagIndex: Int) -> Bool{
+        if flagIndex == currentCountryIndex {
+            self.score += 1
+            return true
+        }else {
+            self.scoreTitle = "Wrong answer, this the flag of \(self.countries[flagIndex])"
+            return false
+        }
+    }
+    
+    func askQuestion() {
+        self.countries.shuffle()
+        self.currentCountryIndex = Int.random(in: 0...2)
     }
 }
 
